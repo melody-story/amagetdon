37fcb1abc3bc4a8ec3f4febaabebd2ca1aee00d8
melody-story <rnlqls7454@naver.com>
2023-02-14 15:05:19 +0900

    Initial Setting

---
+++ src/main/java/com/heeha/amagetdon/AmagetdonApplication.java
@@ -0,0 +1,13 @@
+package com.heeha.amagetdon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmagetdonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmagetdonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/heeha/amagetdon/AmagetdonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heeha.amagetdon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmagetdonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
